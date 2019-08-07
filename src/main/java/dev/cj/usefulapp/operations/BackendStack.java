0100bceb158013596bbdd8f8e78a20e902c6a9c6
Greg Wiley <gwiley@cj.com>
2019-08-07 13:42:14 -0700

    checkpoint

--- src/main/java/dev/cj/usefulapp/operations/BackendStack.java
+++ src/main/java/dev/cj/usefulapp/operations/BackendStack.java
@@ -11,26 +11,22 @@ class BackendStack extends Stack {
     public BackendStack(final Construct parent, final String name) {
         super(parent, name);
 
-        // Code handleOpCode = Code.bucket(codeBucket, handleOpKey);
-        Code handleOpCode = Code.asset("op-handler");
-        FunctionProps handleOpProps = FunctionProps.builder().withHandler("index.handle")
+        final Code handleOpCode = Code.asset("op-handler");
+        final FunctionProps handleOpProps = FunctionProps.builder().withHandler("index.handle")
                 .withRuntime(Runtime.NODEJS_10_X).withCode(handleOpCode).build();
         new Function(this, "HandleOp", handleOpProps);
 
         // // API Gateway not yet supported
-        // CfnResourceProps apiProps =
-        // CfnResourceProps.builder().withType("AWS::ApiGateway::Api")
-        // .withProperties(new HashMap<>()).build();
+        // final CfnResourceProps apiProps = CfnResourceProps.builder().withType("AWS::ApiGateway::Api")
+        //         .withProperties(new HashMap<>()).build();
         // new CfnResource(this, "Api", apiProps);
 
-        // CfnResourceProps resourceProps =
-        // CfnResourceProps.builder().withType("AWS::ApiGateway::Resource")
-        // .withProperties(new HashMap<>()).build();
+        // final CfnResourceProps resourceProps = CfnResourceProps.builder().withType("AWS::ApiGateway::Resource")
+        //         .withProperties(new HashMap<>()).build();
         // new CfnResource(this, "Op", resourceProps);
 
-        // CfnResourceProps methodProps =
-        // CfnResourceProps.builder().withType("AWS::ApiGateway::Method")
-        // .withProperties(new HashMap<>()).build();
+        // final CfnResourceProps methodProps = CfnResourceProps.builder().withType("AWS::ApiGateway::Method")
+        //         .withProperties(new HashMap<>()).build();
         // new CfnResource(this, "PostOp", methodProps);
         // // etc....
 
